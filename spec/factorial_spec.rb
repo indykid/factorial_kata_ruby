a80d4fd4d1f40dbd4d902a10ae3f1aaf24bbd6a1
jarkyn <jarkyn@ymail.com>
2015-04-27 14:28:15 +0100

    removes duplication

--- spec/factorial_spec.rb
+++ spec/factorial_spec.rb
@@ -1,9 +1,6 @@
 def factorial(number)
 	return 1 if number == 0
 	return number*factorial(number - 1)
-	return number*factorial(number - 1)
-	return number*factorial(number - 1)
-	return number*factorial(number - 1)
 end
 
 describe "factorial" do
